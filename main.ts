41f7fda04cb60094766ca18facf76d601d7bf3a9
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-04-02 12:45:27 -0400

    Change and Committed

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
+ * Created by: Yusuf
  * Created on: Sep 2020
- * This program ...
+ * This program uses the distance sensor and save the current distance in cm to a variable.
 */
 
-basic.showString('Hello, World!')
+let distance = 0
+let strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
+
+input.onButtonPressed(Button.A, function () {
+    // save the current distance in cm to a variable
+    distance = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // use that variable to change Neopixel colors
+    if (distance < 10) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+})
